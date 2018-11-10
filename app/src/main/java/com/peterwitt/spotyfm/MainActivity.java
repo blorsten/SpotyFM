5c2922fa3bd597c81095b27515ccef084c0809c3
Peter Witt <peter.witt.joergensen@gmail.com>
2018-11-10 23:22:15 +0100

    Started on app and added OKHTTP

--- app/src/main/java/com/peterwitt/spotyfm/MainActivity.java
+++ app/src/main/java/com/peterwitt/spotyfm/MainActivity.java
@@ -2,12 +2,60 @@ package com.peterwitt.spotyfm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtString;
+    public String url= "https://www.dr.dk/playlister/feeds/nowNext/nowPrev.drxml?items=4&cid=P3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtString= (TextView)findViewById(R.id.MainText);
+
+        try {
+            run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void run() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtString.setText(myResponse);
+                    }
+                });
+            }
+        });
     }
 }
