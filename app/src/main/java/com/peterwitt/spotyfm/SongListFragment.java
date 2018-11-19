8763b8b40f496e47f98bbcf3762e3eeccb6b91ea
Peter Witt <peter.witt.joergensen@gmail.com>
2018-11-19 07:41:46 +0100

    Started on fragments for song list

---
+++ app/src/main/java/com/peterwitt/spotyfm/SongListFragment.java
@@ -0,0 +1,37 @@
+package com.peterwitt.spotyfm;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.peterwitt.spotyfm.RadioAPI.RadioAPIManager;
+
+public class SongListFragment extends Fragment {
+
+    public SongListFragment(){
+        //Required empty constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        inflater.inflate(R.layout.song_list_fragment, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        RecyclerView rv = view.findViewById(R.id.song_list_fragment_recycleView);
+        //Setup list thing
+
+        TextView t = view.findViewById(R.id.song_list_fragment_title);
+        t.setText(RadioAPIManager.getInstance().getCurrentAPI().getName());
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
