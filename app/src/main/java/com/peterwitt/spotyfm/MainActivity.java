f200512dc70a7729a77235d2ae05a0f5d74ad6c8
Peter Witt <peter.witt.joergensen@gmail.com>
2018-11-28 22:54:43 +0100

    Got spotify working

--- app/src/main/java/com/peterwitt/spotyfm/MainActivity.java
+++ app/src/main/java/com/peterwitt/spotyfm/MainActivity.java
@@ -1,5 +1,7 @@
 package com.peterwitt.spotyfm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +10,41 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.peterwitt.spotyfm.RadioAPI.Callbacks.RadioAPIButtonCallback;
 import com.peterwitt.spotyfm.RadioAPI.RadioAPI;
 import com.peterwitt.spotyfm.RadioAPI.RadioAPIManager;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import org.w3c.dom.Text;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.TracksPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String CLIENT_ID = "cb0dc3e4eb424c238e8dec78309e37ae";
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "com.peterwitt.spotyfm://callback";
+
+    private String token;
+
+    SpotifyApi api;
+    SpotifyService service;
+
+    EditText inputText;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +52,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentHandler.getInstance().activity = this;
-        FragmentHandler.getInstance().MakeFragment(R.id.main_activity_frame_layout, new RadioAPIFragment(), true, false);
+        //FragmentHandler.getInstance().MakeFragment(R.id.main_activity_frame_layout, new RadioAPIFragment(), true, false);
+
+        inputText = findViewById(R.id.testEditText);
+        resultText = findViewById(R.id.resultText);
+
+        findViewById(R.id.textButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthenticationRequest request = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+                        .setShowDialog(false)
+                        .setScopes(new String[]{"user-read-email"})
+                        .setCampaign("your-campaign-token")
+                        .build();
+                AuthenticationClient.openLoginActivity(MainActivity.this, REQUEST_CODE, request);
+            }
+        });
+
+        findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(api == null){
+                    api = new SpotifyApi();
+                    api.setAccessToken(token);
+                    service = api.getService();
+                }
+
+                service.searchTracks(inputText.getText().toString(), new Callback<TracksPager>() {
+                    @Override
+                    public void success(TracksPager tracksPager, Response response) {
+                        String text = "";
+
+                        for (int i = 0; i < tracksPager.tracks.items.size(); i++) {
+                            text += tracksPager.tracks.items.get(i).name + "\n";
+                        }
+
+                        resultText.setText(text);
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -40,5 +112,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    token = response.getAccessToken();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView textView = findViewById(R.id.testtext);
+                            textView.setText(response.getAccessToken());
+                        }
+                    });
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView textView = findViewById(R.id.testtext);
+                            textView.setText(response.getError());
+                        }
+                    });
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
     }
 }
